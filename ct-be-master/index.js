7486c2a8590bc60e3b29ef5bf1f2774e10777d9d
jeetungaa <vaasumarothia45@gmail.com>
2024-07-29 22:27:05 +0530

    updated

--- ct-be-master/index.js
+++ ct-be-master/index.js
@@ -4,11 +4,16 @@ const cors=require('cors');
 const { Users, Products, Categories, OrderItems, Review, SubCategories } = require('./db');
 const ProductRouter = require('./routes/product');
 const categoryRouter = require('./routes/category');
+const path = require("path");
+
+
 
 const app=express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
+
 
 app.use('/user',userRouter);
 app.use('/products',ProductRouter);
